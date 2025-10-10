83b9079c8479aded776c10e48ba79698e3bb7401
Forgetallpasswords <rs.khandelwal.gwl@gmail.com>
2025-10-10 11:23:48 +0530

    Home Page update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+// import {get_started} from '../Home/ss.png'
 import './Home.css'
 import { Link } from 'react-router'
 const Home = () => {
   return (
     <>
-    <div className='bg-amber-500 w-full home'>
+    <div className='main'>
 
-      <Link to='/login' className='bg-red-800 login'   >Get Started</Link>
-      
+    <div className='top_image'>
+
     </div>
-    
+    <div className='top_text'>Turn Your sloppy YouTube sessions <br /> into organised study missions.</div>
+    <div className='intro_line'>A centralized hub to track your YouTube learning journey, organise scattered content, distill key insights<br />and navigate your studies with clearity.</div>
+    {/* <button className="get-started-btn" on>
+    </button>     */}
+    <Link to='/login' className='get_started_btn'>
+      {/* <span className="started">STARTED</span>
+      <span className="get">GET</span> */}
+      <img className='text-white' src='../assets/ss.png' alt="get started" />
+    </Link>
+    </div>
     </>
   )
 }
